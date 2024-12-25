029ef8f30b6b13b1cf4c76636bb61b1aa6dd53a8
Numan Ahamed <numanahamedofficial24@gmail.com>
2024-12-25 15:30:25 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Include any global styles if necessary
+import App from './App'; // The root component of your application
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
