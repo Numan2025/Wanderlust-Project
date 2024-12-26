c1e98dd2a1ae46b08209f4ed92dbde55e27c1cfa
Numan Ahamed <numanahamedofficial24@gmail.com>
2024-12-26 07:29:19 +0530

    Updated the project with new files and modifications

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // Include any global styles if necessary
-import App from './App'; // The root component of your application
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
